12c2cd8b0684f1d37f2dd42978fc754bf4e4794a
nidansuyandex-life <nidansu.yandex@gmail.com>
2026-09-23 16:48:02 +0800

    Delete build.js

--- build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const src = path.join(__dirname, 'index.html');
-const dist = path.join(__dirname, 'dist');
-
-fs.mkdirSync(dist, { recursive: true });
-fs.copyFileSync(src, path.join(dist, 'index.html'));
-console.log('健康生活 App web assets copied to dist/');
