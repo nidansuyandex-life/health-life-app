d9e7721e6d7aadd170a7f40db86e33164f2043b0
nidansuyandex-life <nidansu.yandex@gmail.com>
2026-09-24 09:24:02 +0800

    Delete build.js

--- build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const root = __dirname;
-const dist = path.join(root, 'dist');
-fs.rmSync(dist, { recursive: true, force: true });
-fs.mkdirSync(dist, { recursive: true });
-fs.copyFileSync(path.join(root, 'index.html'), path.join(dist, 'index.html'));
-console.log('Web app copied to dist/');
